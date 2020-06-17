14fca7b10ba554a6946fcd50abf598a2afa0e9dc
wagner trindade <lejolupo@gmail.com>
2020-06-17 17:26:12 -0300

    adição do challenge 1 do curso udemy

---
+++ src/main/kotlin/academy/learnprogramming/challenge1/Challenge1.kt
@@ -0,0 +1,33 @@
+package academy.learnprogramming.challenge1
+
+fun main(args: Array<String>) {
+
+    //exemplos de type inference
+    val hello1 = "Hello"
+    val hello2 = "Hello"
+
+    println("hello1 e hello2 são referencialmente iguais? ${hello1 === hello2}")
+
+    println("hello1 e hello2 são estruturalmente iguais? ${hello1 == hello2}")
+
+    var numero = 2988
+
+    //exemplo do tipo Any e smart cast
+    val qualquerValor: Any = "The Any type is the root of the Kotlin class hierarchy"
+
+    if (qualquerValor is String)
+        println(qualquerValor.toUpperCase())
+
+    //exemplo de tripple quotted string
+    println("""   1
+        |  11
+        | 111
+        |1111
+    """.trimMargin())
+
+    println("""1   1
+        1  11
+        1 111
+        11111
+    """.trimMargin("1"))
+}
